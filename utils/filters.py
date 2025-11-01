fa051c0cbfdbbfdf432849fb391cf8feb6091eef
MIAO <not.alice.not@gmail.com>
2025-11-01 10:44:06 +0300

    9

--- utils/filters.py
+++ utils/filters.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime, timedelta
-from utils.utils import is_image_nsfw, get_file_age_days
+from utils.ai_filter import is_image_nsfw, get_file_age_days
 import config
 from typing import List
 
--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from datetime import datetime, timedelta
-from ai_filter import detect_nsfw
+from utils.ai_filter import detect_nsfw
 import config
 
 async def download_file(bot, file_id, dest_path):
